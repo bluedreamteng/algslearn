e1d5321358d9617e7fd002ceaa1b5ba42bef4329
tenglovemeng <1334500819@qq.com>
2021-06-27 23:02:09 +0800

    1.3习题解答

--- src/chapter1/section3/Exerise25.java
+++ src/chapter1/section3/Exerise25.java
@@ -2,6 +2,17 @@ package chapter1.section3;
 
 public class Exerise25 {
     public static void insertAfter(Node node1,Node node2) {
-
+        if(node1 == null || node2 == null) {
+            //do nothing
+        } else {
+            Node oldNode1Next = node1.next;
+            //找到node2末尾节点
+            Node node2Last = node2;
+            while (node2Last.next != null) {
+                node2Last = node2Last.next;
+            }
+            node2Last.next = oldNode1Next;
+            node1.next = node2;
+        }
     }
 }
